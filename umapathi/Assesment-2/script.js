6cb47cb4dc2d147e16d2f59e864b4abf225a28a2
umapathi <umapathi.krishnan@nutechnologyinc.com>
2022-06-21 14:30:50 +0530

    assesment -2 and first react app

--- umapathi/Assesment-2/script.js
+++ umapathi/Assesment-2/script.js
@@ -11,39 +11,64 @@ const validate = ()=>{
     let dob = document.myform.dob.value;
     let password = document.myform.password.value;
     let cpassword = document.myform.cpassword.value;
-    let errDiv = document.getElementsByClassName("err");
+    let errDiv = document.getElementById("err");
    let fanameErr = document.getElementById("fname-err");
-   let lnameErr = document.getElementById("laname-err");
+   let lnameErr = document.getElementById("lname-err");
    let emailErr = document.getElementById("email-err");
    let mobileErr =document.getElementById("mobile-err");
-   let passwordErr = document.getElementById("passworrd-err");
+   let passwordErr = document.getElementById("password-err");
    let cPasswordErr = document.getElementById("cpassworrd-err");
 
-//    style of error div
-
+   fanameErr.style.display ="none";
     // validation part
     // first name should notbe empty
     if(fname.length >0 && lname.length>0 && email.length >0 && mobile.length === 10 && password.length >=5 && password == cpassword){
+        alert("Registerd succesfully!")
         return true;
        }
+       if(!(fname.length >0 && lname.length>0 && email.length >0 && mobile.length === 10 && password.length >=5 && password == cpassword)){
+        errDiv.style.display="block";
+        errDiv.style.backgroundColor="red";
+        errDiv.style.color="white";
+        document.getElementById("err").innerHTML="Please fill the form correctly !";
+        
+       }
     if(fname.length <=0){
+        fanameErr.style.display="block";
+        fanameErr.style.backgroundColor="red";
+        fanameErr.style.color="white";
         document.getElementById("fname-err").innerHTML="First name should not be empty!";
 
     }
     // last name shoould not be empty
-     if(lname.length <=0){
+    else if(lname.length <=0){
+        lnameErr.style.display="block";
+        lnameErr.style.backgroundColor="red";
+        lnameErr.style.color="white";
         document.getElementById("lname-err").innerHTML="Last name should not be empty!";
     }
-     if(email.length <=0){
+    else if(email.length <=0){
+        emailErr.style.display="block";
+        emailErr.style.backgroundColor="red";
+        emailErr.style.color="white";
         document.getElementById("email-err").innerHTML = "email should not be empty";
     }
-    if(mobile.length <=0 || mobile.length >10){
+    else if(mobile.length <=9 ){
+        mobileErr.style.display="block";
+        mobileErr.style.backgroundColor="red";
+        mobileErr.style.color="white";
         document.getElementById("mobile-err").innerHTML = "Mobile number should be in 10 numbers!"
     }
-    if(password.length<5){
+    else if(password.length<5){
+        passwordErr.style.display="block";
+        passwordErr.style.backgroundColor="red";
+        passwordErr.style.color="white";
         document.getElementById("password-err").innerHTML = "Password must be atleast 5 characters long!"
     }
-    if(password != cpassword){
+    else if(password != cpassword){
+        cPasswordErr.style.display="block";
+        cPasswordErr.style.backgroundColor="red";
+        cPasswordErr.style.color="white";
         document.getElementById("cpassword-err").innerHTML = "Incorrect password!"
     }
     return false;
