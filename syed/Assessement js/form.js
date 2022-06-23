4f5ba2f2c7d5f16476251401cb18c4941d270f60
syed <syed.thajudeen@nutechnologyinc.com>
2022-06-23 08:14:27 +0530

    updated

--- syed/Assessement js/form.js
+++ syed/Assessement js/form.js
@@ -1,63 +1,93 @@
-
 function validate(e)
 {
     e.preventDefault();
-    const fname = document.querySelector('#fname').value;
-    const lname = document.querySelector('#lname').value;
-    const email = document.querySelector('#email').value;
-    const phoneNumber = document.querySelector('#phone').value;
-    const dob = document.querySelector('#birthday').value;   
-    const password = document.querySelector('#password').value;
-    const confirmPassword = document.querySelector('#confirm-password').value;
 
-    const fnameError = document.querySelector('#fnameError');
-    const lnameError = document.querySelector('#lnameError');
-    const emailError = document.querySelector('#emailError');
-    const phoneNumberError = document.querySelector('#phoneError');
-    const dobError = document.querySelector('#dobError');   
-    const passwordError = document.querySelector('#passwordError');
-    const confirmPasswordError = document.querySelector('#confirmPasswordError');
-    if(fname === '' ){
-        fnameError.innerHTML = 'cannot be empty'
-    }
-    else{
-        fnameError.innerHTML = ''
-    }
+    var fname = document.getElementById('fname').value;
+    var lname = document.getElementById('lname').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var dob = document.getElementById('dob');
+    var password = document.getElementById('password').value;
+    var confirmpassword= document.getElementById('confirmpassword').value;
 
-    if(lname === ''){
-        lnameError.innerHTML = 'cannot be empty'
-    }  else{
-        lnameError.innerHTML = ''
+
+    var fnameError = document.getElementById('fnameError');
+    var lnameError = document.getElementById('lnameError');
+    var emailError = document.getElementById('emailError');
+    var phoneError = document.getElementById('phoneError');
+    var dobError = document.getElementById('dobError');
+    var passwordError = document.getElementById('passwordError');
+    var confirmpasswordError = document.getElementById('confirmPasswordError');
+
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var phnNumberExpression = /^\d{10}$/;
+    if(fname === '')
+    {
+        fnameError.style.display = "inline";
+        fnameError.innerHTML = " please fill the fname field";
+       
     }
-    
-    if(email === ''){
-        emailError.innerHTML = 'cannot be empty'
-    }  else{
+    else {
+        fnameError.style.display = "none";
+        fnameError.innerHTML  = ''
+    }
+    if(lname === '')
+    {
+        lnameError.style.display = "inline";
+        lnameError.innerHTML = " please fill the lname field";
+       
+    }
+    else {
+        lnameError.style.display = "none";
+        lnameError.innerHTML  = ''
+    }
+    if(email === '')
+    {
+        emailError.style.display = "inline";
+        emailError.innerHTML = " please fill the emailid";
+    }
+    else 
+    {
+        emailError.style.display = "none";
         emailError.innerHTML = ''
     }
-    
-    if(phoneNumber === ''){
-        phoneNumberError.innerHTML = 'cannot be empty'
-    } else{
-        phoneNumberError.innerHTML = ''
+    if(phone.match(phnNumberExpression))
+    {
+        phoneError.style.display = "none";
+        phoneError.innerHTML = '';
     }
-    
-    if(dob === ''){
-        dobError.innerHTML = 'cannot be empty'
-    }else{
-        dobError.innerHTML = ''
+     else 
+     {
+        phoneError.style.display = "inline";
+        phoneError.innerHTML = " please fill 10 digit number";
+     }
+      if(password === '')
+      {
+        passwordError.style.display = "inline";
+        passwordError.innerHTML = "password cannot be empty";
     }
-    
-    if(password === ''){
-        passwordError.innerHTML = 'cannot be empty'
-    }else{
+     else if (password.length < 5) {
+        console.log("less than 5");
+        passwordError.style.display = "inline";
+        passwordError.innerHTML ="password atleast 6 characters";
+    }
+     else 
+     {
+        passwordError.style.display = "none";
         passwordError.innerHTML = ''
+     }
+     if(confirmpassword === password)
+     {
+        confirmpasswordError.style.display = "none";
+        confirmpasswordError.innerHTML = ''
     }
+     else 
+     {
+        confirmpasswordError.style.display = "inline";
+        confirmpasswordError.innerHTML ="password mismatch";
+     }
+    }
+
     
-    if(confirmPassword === ''){
-        confirmPasswordError.innerHTML = 'cannot be empty'
-    }else{
-        confirmPasswordError.innerHTML = ''
-    }
-  
-}
+    
+
