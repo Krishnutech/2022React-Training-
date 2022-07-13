131aa0169d02a942def336d21774ec26a428030f
asraf <mohammed.asrafali@nutechnologyinc.com>
2022-07-13 15:38:48 +0530

    Task

---
+++ Asraf/thukk/src/components/index.js
@@ -0,0 +1,45 @@
+
+import React from 'react';
+import {fetchBlogs} from '../actions';
+import {connect} from 'react-redux';
+class Blogs extends React.Component {
+
+    componentDidMount = () => {
+        console.log('mounted')
+        this.props.fetchBlogs()
+    }
+    render() {
+        return this.props.loading ? 'please wait for a moment while your data is processing....' : ( 
+
+            <table>
+            <thead>
+                <tr>
+                    <th>S.no</th>
+                    <th>Title</th>
+                </tr>
+            </thead>
+            <tbody>
+                {this.props.blogs.map(blog =>{
+                    return <tr><td>{blog.id}</td><td>{blog.title}</td></tr>
+                })
+            }
+                </tbody>           
+                 </table>)
+
+
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        blogs: state.blogs,
+        loading:state.loading
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+        return {
+            fetchBlogs: () => dispatch(fetchBlogs())
+                }
+            }
+        export default connect(mapStateToProps,mapDispatchToProps)(Blogs)
